f5e00c9e17b486c8ba57d2b152f8b2631f841483
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-09-22 22:47:27 -0500

    Added header

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -8,47 +8,49 @@ export const NavBar = (props) => {
   };
 
   return (
-    <ul className="navbar">
-      <li className="navbar__item">
-        <Link to={"/my-recipes"}>My Recipes</Link>
-      </li>
-      <li className="navbar__item">
-        <Link to={"/recipes"}>Browse Recipes</Link>
-      </li>
-      <li className="navbar__item">
-        <Link to={"/recipes/new"}>Create a Recipe</Link>
-      </li>
-      <li className="navbar__item">
-        <Link to={"/restaurants"}>Restaurants</Link>
-      </li>
-      <li className="navbar__item">
-        <Link to={"/"}>About</Link>
-      </li>
-      {localStorage.getItem("bt_token") !== null ? (
+    <>
+      <ul className="navbar">
         <li className="navbar__item">
-          <Link
-            onClick={() => {
-              logout();
-            }}
-            to={"/"}
-          >
-            Logout
-          </Link>
+          <Link to={"/my-recipes"}>My Recipes</Link>
         </li>
-      ) : (
-        <>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">
-              Login
+        <li className="navbar__item">
+          <Link to={"/recipes"}>Browse Recipes</Link>
+        </li>
+        <li className="navbar__item">
+          <Link to={"/recipes/new"}>Create a Recipe</Link>
+        </li>
+        <li className="navbar__item">
+          <Link to={"/restaurants"}>Restaurants</Link>
+        </li>
+        <li className="navbar__item">
+          <Link to={"/"}>About</Link>
+        </li>
+        {localStorage.getItem("bt_token") !== null ? (
+          <li className="navbar__item">
+            <Link
+              onClick={() => {
+                logout();
+              }}
+              to={"/"}
+            >
+              Logout
             </Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </li>
-        </>
-      )}{" "}
-    </ul>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+            </li>
+          </>
+        )}{" "}
+      </ul>
+    </>
   );
 };
