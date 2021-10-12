250a8455d01ea9568da359af497b76730d94c88c
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-10-12 12:18:58 -0500

    Fixed navbars, styled recipes

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
@@ -13,13 +13,83 @@ export const NavBar = () => {
   const [showNavBar, setShowNavBar] = useState(false);
   const toggleNavBar = () => setShowNavBar(!showNavBar);
 
+  const [isMobile, setMobile] = useState(window.innerWidth < 600);
+
+  const updateMedia = () => {
+    setMobile(window.innerWidth < 600);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <>
-      {showNavBar ? (
+      {isMobile ? (
         <>
-          <div className="navbar-collapse" onClick={() => toggleNavBar()}>
-            <ArrowDropUpIcon />
-          </div>
+          {showNavBar ? (
+            <>
+              <div className="navbar-collapse" onClick={() => toggleNavBar()}>
+                <ArrowDropUpIcon />
+              </div>
+              <ul className="navbar">
+                <li className="navbar__item">
+                  <Link to={"/my-recipes"}>My Recipes</Link>
+                </li>
+                <li className="navbar__item">
+                  <Link to={"/recipes"}>Browse Recipes</Link>
+                </li>
+                <li className="navbar__item">
+                  <Link to={"/recipes/new"}>Create a Recipe</Link>
+                </li>
+                <li className="navbar__item">
+                  <Link to={"/restaurants"}>Restaurants</Link>
+                </li>
+                <li className="navbar__item">
+                  <Link to={"/restaurants/favorites"}>Favorites</Link>
+                </li>
+                <li className="navbar__item">
+                  <Link to={"/about"}>About</Link>
+                </li>
+                {localStorage.getItem("bt_token") !== null ? (
+                  <li className="navbar__item">
+                    <Link
+                      onClick={() => {
+                        logout();
+                      }}
+                      to={"/"}
+                    >
+                      Logout
+                    </Link>
+                  </li>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/login">
+                        Login
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/register">
+                        Register
+                      </Link>
+                    </li>
+                  </>
+                )}{" "}
+              </ul>
+            </>
+          ) : (
+            // If show navbar is false, display nothing. Tiny little header.
+            <>
+              <div className="navbar-expand" onClick={() => toggleNavBar()}>
+                <ArrowDropDownIcon />
+              </div>
+            </>
+          )}
+        </>
+      ) : (
+        <>
           <ul className="navbar">
             <li className="navbar__item">
               <Link to={"/my-recipes"}>My Recipes</Link>
@@ -65,13 +135,6 @@ export const NavBar = () => {
               </>
             )}{" "}
           </ul>
-        </>
-      ) : (
-        // If show navbar is false, display nothing. Tiny little header.
-        <>
-          <div className="navbar-expand" onClick={() => toggleNavBar()}>
-            <ArrowDropDownIcon />
-          </div>
         </>
       )}
       {/* END OF NAVBAR */}
